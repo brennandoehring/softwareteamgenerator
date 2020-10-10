676b15d69a6adebc87da4a87691be861be832194
Brennan Doehring <brennandoehringd@outlook.com>
2020-10-10 08:40:58 -0400

    Wrote prompt for employee.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const iquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 const Employee = require("..\softwareteamgenerator\lib\Employee");
 const Engineer = require("..\softwareteamgenerator\lib\Engineer");
 const Intern = require("..\softwareteamgenerator\lib\Intern");
@@ -9,5 +10,31 @@ const generateEmployee = require("..\softwareteamgenerator\templates\employee");
 const generateEngineer = require("..\softwareteamgenerator\templates\engineer");
 const generateIntern = require("..\softwareteamgenerator\templates\intern");
 const generateManager = require("..\softwareteamgenerator\templates\manager");
+const inquirer = require("inquirer");
 const employeeList = [];
 
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter employee name"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Enter employee id number"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter employee email"
+        },
+        {
+            type: "list",
+            name: "role",
+            message: "What role is this employee?",
+            choices: ["Engineer", "Intern", "Manager"]
+        }
+    ])
+}
