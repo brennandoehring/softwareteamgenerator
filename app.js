60c243c58a69796cdf45413e9fb29fe9b258587c
Brennan Doehring <brennandoehringd@outlook.com>
2020-10-13 21:43:38 -0400

    HTML generating properly.

--- app.js
+++ app.js
@@ -1,17 +1,69 @@
-const iquirer = require("inquirer");
-const fs = require("fs");
-const util = require("util");
-const writeFileAsync = util.promisify(fs.writeFile);
-const Employee = require("..\softwareteamgenerator\lib\Employee");
-const Engineer = require("..\softwareteamgenerator\lib\Engineer");
-const Intern = require("..\softwareteamgenerator\lib\Intern");
-const Manager = require("..\softwareteamgenerator\lib\Manager");
-const generateEmployee = require("..\softwareteamgenerator\templates\employee");
-const generateEngineer = require("..\softwareteamgenerator\templates\engineer");
-const generateIntern = require("..\softwareteamgenerator\templates\intern");
-const generateManager = require("..\softwareteamgenerator\templates\manager");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
 const employeeList = [];
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+const Employee = require("./lib/Employee");
+
+createEmployee();
+
+function createEmployee() {
+    promptUser().then(function (value) {
+        let emp = new Employee(value.name, value.id, value.email);
+        if (value.role === "Engineer") {
+            promptEngineer().then(function (value) {
+                let eng = new Engineer(emp.name, emp.id, emp.email, value.github);
+                employeeList.push(eng);
+                if (value.anotherEmployee === "Yes") {
+                    createEmployee();
+                } else {
+                    fs.writeFile(outputPath, render(employeeList), function(err) {
+                        if (err) {
+                            throw err;
+                        }
+                    });
+                }
+            });
+        } 
+        else if (value.role === "Intern") {
+            promptIntern().then(function (value) {
+                let int = new Intern(emp.name, emp.id, emp.email, value.school);
+                employeeList.push(int);
+                if (value.anotherEmployee === "Yes") {
+                    createEmployee();
+                } else {
+                    fs.writeFile(outputPath, render(employeeList), function(err) {
+                        if (err) {
+                            throw err;
+                        }
+                    });
+                }
+            });
+        }
+        else {
+            promptManager().then(function (value) {
+                let man = new Manager(emp.name, emp.id, emp.email, value.officeNumber);
+                employeeList.push(man);
+                if (value.anotherEmployee === "Yes") {
+                    createEmployee();
+                } else {
+                    fs.writeFile(outputPath, render(employeeList), function(err) {
+                        if (err) {
+                            throw err;
+                        }
+                    });
+                }
+            });
+        }
+    });
+}
 
 function promptUser() {
     return inquirer.prompt([
@@ -45,6 +97,12 @@ function promptEngineer() {
             type: "input",
             name: "github",
             message: "What is this engineer's github profile?"
+        },
+        {
+            type: "list",
+            name: "anotherEmployee",
+            message: "Would you like to add another employee?",
+            choices: ["Yes", "No"]
         }
     ]);
 }
@@ -55,6 +113,12 @@ function promptIntern() {
             type: "input",
             name: "school",
             message: "What school does this intern attend?"
+        },
+        {
+            type: "list",
+            name: "anotherEmployee",
+            message: "Would you like to add another employee?",
+            choices: ["Yes", "No"]
         }
     ]);
 }
@@ -65,6 +129,12 @@ function promptManager() {
             type: "input",
             name: "officeNumber",
             message: "What office number does this manager reside?"
+        },
+        {
+            type: "list",
+            name: "anotherEmployee",
+            message: "Would you like to add another employee?",
+            choices: ["Yes", "No"]
         }
     ]);
 }
